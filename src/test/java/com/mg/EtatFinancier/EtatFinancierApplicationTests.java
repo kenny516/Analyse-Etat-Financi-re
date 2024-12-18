e78a0e285e1e430a445c1f1eb53fc60cbebacadd
Andriantsirafy chan kenny <kennyandriantsirafychan@gmail.com>
2024-12-18 19:17:15 +0300

    init project spring boot

---
+++ src/test/java/com/mg/EtatFinancier/EtatFinancierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.EtatFinancier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtatFinancierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
